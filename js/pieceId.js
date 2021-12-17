be0363d5f32a4c74bd179b1a4a5ced720206d371
Freddy Jiang <jtf.imperial@gmail.com>
2021-12-17 18:37:54 +0000

    Write Rotation and PieceID enums

---
+++ js/pieceId.js
@@ -0,0 +1,9 @@
+class PieceID {
+  static get T() { return "t"; }
+  static get J() { return "j"; }
+  static get Z() { return "z"; }
+  static get O() { return "o"; }
+  static get S() { return "s"; }
+  static get L() { return "l"; }
+  static get I() { return "i"; }
+}
---
+++ js/rotation.js
@@ -0,0 +1,6 @@
+class Rotation {
+  static get DOWN()  { return 0; }
+  static get LEFT()  { return 1; }
+  static get UP()    { return 2; }
+  static get RIGHT() { return 3; }
+}
